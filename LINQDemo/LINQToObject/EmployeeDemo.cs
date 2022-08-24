7d1110ebb62805e34e1a132627597fd4ea0646af
bhagyashrilavhe <bhagyashrilavhe66@gmail.com>
2022-08-24 18:00:53 +0530

    LINQToObject

---
+++ LINQDemo/LINQToObject/EmployeeDemo.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQDemo.LINQToObject
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string City { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id} {Name} {Salary} {City} ";
+        }
+    }
+    class EmployeeDemo
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> emp = new List<Employee>()
+            {
+                new Employee {Id=1,Name="Bhagyashri",Salary=30000,City="Pune" },
+                new Employee {Id=2,Name="Kartik",Salary=40000,City="Mumbai"},
+                new Employee {Id=3,Name="Rutuja",Salary=35000,City="Pune"},
+                new Employee {Id=4,Name="Omkar",Salary=45000,City="Kolkata"},
+                new Employee {Id=5,Name="Sagar",Salary=34000,City="Baramati"},
+                new Employee {Id=6,Name="Priya",Salary=44000,City="Pune"},
+                new Employee {Id=7,Name="Pratiksha",Salary=55000,City="Mumbai"},
+            };
+
+            //LINQ Query
+
+            //1. display all employees
+            var result1 = from e in emp
+                          select e;
+
+            // 2.display employee with asending order by name
+            var result2 = from e in emp
+                          orderby e.Name ascending
+                          select e;
+
+            // 3.display employee whose salary is > 25000
+            var result3 = from e in emp
+                          where e.Salary > 25000
+                          select e;
+
+            //4.display employee whos from 'Pune' City
+            var result4 = from e in emp
+                          where e.City == "Pune"
+                          select e;
+
+            //5. display employee with desending order by salary
+            var result5 = from e in emp
+                          orderby e.Salary descending
+                          select e;
+
+            //6. display employee whose name start with 'P'
+            var result6 = from e in emp
+                          where e.Name.StartsWith("P")
+                          select e;
+
+            //7. display employee whose salary is > 25000 & emp is from 'Mumbai' city
+            var result7 = from e in emp
+                          where e.Salary > 2500 && e.City.Contains("Mumbai")
+                          select e;
+
+            // ************Lambda Expression*******************
+
+            // 1.display all employees
+            var res = emp.ToList();
+
+            //2.display employee with asending order by name
+            var res1 = emp.OrderBy(e => e.Name).ToList();
+
+            //3.display employee whose salary is > 25000
+            var res2 = emp.Where(e => e.Salary > 25000).ToList();
+
+            //4.display employee whos from 'Pune' City
+            var res3 = emp.Where(e => e.City == "Pune").ToList();
+
+            //5.display employee with desending order by salary
+            var res4 = emp.OrderByDescending(e => e.Salary).ToList();
+
+            //6.display employee whose name start with 'P'
+            var res5 = emp.Where(e => e.Name.StartsWith("P")).ToList();
+
+            //7.display employee whose salary is > 25000 & emp is from 'Mumbai' city
+            var res6 = emp.Where(e => e.Salary > 25000 && e.City.Contains("Mumbai")).ToList();
+
+
+
+            foreach (Employee item in res6)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ LINQDemo/LINQToObject/ProductDemo1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQDemo.LINQToObject
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string CompanyName { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id} -> {Name} -> {Price} -> {CompanyName}";
+        }
+
+
+    }
+    class ProductDemo1
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product { Id = 1, Name= "Mouse",Price=799,CompanyName="Dell"},
+                new Product { Id = 2, Name = "Mouse", Price = 699, CompanyName = "Lenovo" },
+                new Product { Id = 3, Name = "Laptop", Price = 34799, CompanyName = "Dell" },
+                new Product { Id = 4, Name = "Laptop", Price = 45799, CompanyName = "Sony" },
+                new Product { Id = 5, Name = "Laptop", Price = 38799, CompanyName = "Lenovo" },
+                new Product { Id = 6, Name = "Keyboard", Price = 599, CompanyName = "Dell" },
+                new Product { Id = 7, Name = "Keyboard", Price = 999, CompanyName = "Microsoft" },
+                new Product { Id = 8, Name = "RAM 4GB", Price = 2799, CompanyName = "Intel" },
+                new Product { Id = 9, Name = "Speaker", Price = 5799, CompanyName = "Sony" },
+                new Product { Id = 10, Name = "USB Mouse", Price = 1299, CompanyName = "Dell" },
+            };
+
+            // LINQ Query
+
+            var result1 = from p in products
+                          select p;
+
+            var result2 = from p in products
+                          where p.Price < 2000
+                          select p;
+
+            var result3 = from p in products
+                          where p.Price > 2000 && p.Price < 3000
+                          select p;
+
+            var result4 = from p in products
+                          where p.CompanyName.StartsWith("D")
+                          select p;
+
+            var result5 = from p in products
+                          where p.CompanyName.EndsWith("o")
+                          select p;
+
+            var result6 = from p in products
+                          where p.CompanyName.Contains("m") || p.CompanyName.Contains("M")
+                          select p;
+
+            var result7 = from p in products
+                          where p.Price < 2500
+                          orderby p.Price descending
+                          select p;
+
+            // Lambda expression
+
+            var res = products.Where(p => p.Price < 2000).ToList();
+
+            var res2 = products.Where(p => p.Price > 2000).OrderBy(x => x.Name).ToList();
+
+            // retrive single record
+            var res3 = products.Where(x => x.Id == 1).SingleOrDefault();
+
+            var res4 = products.Where(x => x.Price < 2500).OrderBy(x => x.Price).ToList().Take(3);
+
+            var res5 = products.Where(x => x.Price < 2500).OrderBy(x => x.Price).ToList().Skip(3);
+
+
+
+            foreach (Product item in res5)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
